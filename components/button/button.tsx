0457305dd9667b24b6b7921546ce8c158817c4c1
Juan Jose Borrelli <jjborrelli.secondary@gmail.com>
2022-11-25 12:04:24 -0300

    delete extra text

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -21,8 +21,7 @@ export const Button = ({
       {...restProps}
     >
       {children}
-      {variant === "rectangle" && active && <LowerLine />} extra text [add more
-      extra text]
+      {variant === "rectangle" && active && <LowerLine />} extra text
     </BaseButton>
   );
 };
